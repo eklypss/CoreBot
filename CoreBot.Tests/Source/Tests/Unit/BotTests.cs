d47f4c10a1587dd315aec8861a702df65d692ddf
Alex <dwt416@gmail.com>
2017-05-18 16:55:48 +0300

    Created project for tests

---
+++ CoreBot.Tests/Source/Tests/Unit/BotTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CoreBot.Tests.Unit
+{
+    public class BotTests
+    {
+        [Fact]
+        public void ConnectionTest()
+        {
+            // TODO: connection tests
+        }
+    }
+}
