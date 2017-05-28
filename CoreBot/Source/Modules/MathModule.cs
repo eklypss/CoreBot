1afdf24fe1064efdf286731e31b4655d54db583b
Alex <dwt416@gmail.com>
2017-05-28 14:33:58 +0300

    Added simple math module

---
+++ CoreBot/Source/Modules/MathModule.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace CoreBot.Modules
+{
+    [Group("math"), Summary("Basic math commands.")]
+    public class MathModule : ModuleBase
+    {
+        [Command("add")]
+        public async Task Add(int i, int i2)
+        {
+            await ReplyAsync($"{i} + {i2} = {i + i2}");
+        }
+
+        [Command("sub")]
+        public async Task Substract(int i, int i2)
+        {
+            await ReplyAsync($"{i} - {i2} = {i - i2}");
+        }
+
+        [Command("multiply")]
+        public async Task Multiply(int i, int i2)
+        {
+            await ReplyAsync($"{i} * {i2} = {i * i2}");
+        }
+    }
+}
