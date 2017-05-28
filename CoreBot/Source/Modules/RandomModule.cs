ae54e32c1d6ee2f57017ff81f3265ba4c439500c
Alex <dwt416@gmail.com>
2017-05-28 20:07:07 +0300

    Added random module

---
+++ CoreBot/Source/Modules/RandomModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace CoreBot.Source.Modules
+{
+    public class RandomModule : ModuleBase
+    {
+        [Command("random"), Summary("Generates a random number between two given values.")]
+        public async Task Random(int firstNumber, int secondNumber)
+        {
+            Random random = new Random();
+            await ReplyAsync($"{random.Next(firstNumber, secondNumber)}");
+        }
+    }
+}
