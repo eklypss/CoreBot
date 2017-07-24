bfc91f6b41539d54ca6e639bf0dc4f93625b73a7
eklypss <eklypssdev@gmail.com>
2017-07-24 16:06:39 +0300

    Changed IDisposable implementation

--- CoreBot/Source/Modules/MathModule.cs
+++ CoreBot/Source/Modules/MathModule.cs
@@ -10,10 +10,11 @@ namespace CoreBot.Modules
         [Command("math"), Summary("Calculates basic math operations.")]
         public async Task Calculate([Remainder] string input)
         {
-            var dataTable = new DataTable() { CaseSensitive = false, Locale = CultureInfo.CurrentCulture };
-            var result = dataTable.Compute(input, string.Empty);
-            await ReplyAsync($"`{input} = {result}`");
-            dataTable.Dispose();
+            using (var dataTable = new DataTable() { CaseSensitive = false, Locale = CultureInfo.CurrentCulture })
+            {
+                var result = dataTable.Compute(input, string.Empty);
+                await ReplyAsync($"`{input} = {result}`");
+            }
         }
     }
 }
