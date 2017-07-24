3ac070f1f806f66a8519ef6170fdeba4f45b59ce
eklypss <eklypssdev@gmail.com>
2017-07-24 16:23:47 +0300

    Added custom exception

---
+++ CoreBot/Source/Exceptions/CoreBotException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CoreBot.Exceptions
+{
+    [Serializable]
+    public class CoreBotException : Exception
+    {
+        public CoreBotException() : base()
+        {
+        }
+
+        public CoreBotException(string message) : base(message)
+        {
+        }
+
+        public CoreBotException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CoreBotException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
