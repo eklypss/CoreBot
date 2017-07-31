e78599a156380782c6f702070c3677f6f8d5d0cc
eklypss <eklypssdev@gmail.com>
2017-07-31 16:37:25 +0300

    Add EmoteModule

---
+++ CoreBot/Source/Modules/EmoteModule.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace CoreBot.Modules
+{
+    [Group("emote"), Summary("Module for listing custom server emotes.")]
+    public class EmoteModule : ModuleBase
+    {
+        [Command("find")]
+        public async Task GetEmotes(string searchTerm)
+        {
+            var emotes = Context.Guild.Emotes.Where(x => x.Name.Contains(searchTerm));
+            var emoteNames = new List<string>();
+            foreach (var emote in emotes)
+            {
+                emoteNames.Add($"<:{emote.Name}:{emote.Id}>");
+            }
+            if (emoteNames.Count > 0) await ReplyAsync(string.Join(" ", emoteNames));
+            else await ReplyAsync("No matches.");
+        }
+
+        [Command("list")]
+        [Alias("all")]
+        public async Task ListEmotes()
+        {
+            var emoteNames = new List<string>();
+            foreach (var emote in Context.Guild.Emotes)
+            {
+                emoteNames.Add($"<:{emote.Name}:{emote.Id}>");
+            }
+            if (emoteNames.Count > 0) await ReplyAsync(string.Join(" ", emoteNames));
+            else await ReplyAsync("This server has no custom emotes.");
+        }
+    }
+}
