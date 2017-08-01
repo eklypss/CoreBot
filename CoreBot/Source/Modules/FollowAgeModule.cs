f50adc195959d7c6fd2c3115db752db2871a0cee
eklypss <eklypssdev@gmail.com>
2017-08-01 19:07:13 +0300

    Add FollowAgeModule

---
+++ CoreBot/Source/Modules/FollowAgeModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using CoreBot.Settings;
+using Discord.Commands;
+
+namespace CoreBot.Modules
+{
+    public class FollowAgeModule : ModuleBase
+    {
+        [Command("followage")]
+        public async Task GetFollowAge(string user, string channel)
+        {
+            using (var http = new HttpClient())
+            {
+                var result = await http.GetAsync(string.Format(DefaultValues.FOLLOWAGE_URL, user, channel));
+                await ReplyAsync(await result.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
--- CoreBot/Source/Settings/DefaultValues.cs
+++ CoreBot/Source/Settings/DefaultValues.cs
@@ -13,5 +13,6 @@
         public const string FMI_URL = "http://ilmatieteenlaitos.fi/saa/{0}?forecast=short";
         public const string FMI_TEMP_URL = "http://ilmatieteenlaitos.fi/observation-data?station=";
         public const string OPEN_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}";
+        public const string FOLLOWAGE_URL = "https://2g.be/twitch/following.php?user={0}&channel={1}&format=mwdhms";
     }
 }
