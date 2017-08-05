c4faf09652a011172fa5f113798a9f717a57a36f
eklypss <eklypssdev@gmail.com>
2017-08-06 01:00:43 +0300

    Add RemoteModule

---
+++ CoreBot/Source/Modules/RemoteModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using CoreBot.Settings;
+using Discord.Commands;
+
+namespace CoreBot.Modules
+{
+    public class RemoteModule : ModuleBase
+    {
+        [Command("remote")]
+        public async Task SendRemoteMessage([Remainder] string message)
+        {
+            var guilds = await Context.Client.GetGuildsAsync();
+            var textChannels = await guilds.FirstOrDefault(x => x.Name == BotSettings.Instance.DefaultGuild).GetTextChannelsAsync();
+            var channel = textChannels.FirstOrDefault(x => x.Name == BotSettings.Instance.DefaultChannel);
+            await channel.SendMessageAsync(message);
+        }
+    }
+}
