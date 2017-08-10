6d4822cac2b2d3780c12939f99dc31c88e8ca53a
eklypss <eklypssdev@gmail.com>
2017-08-10 18:58:07 +0300

    Add LinkModule

---
+++ CoreBot/Source/Modules/LinkModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace CoreBot.Modules
+{
+    /// <summary>
+    /// Allows lazy users to link specific links (e.g. Twitch channels) fast, without having to write
+    /// or copy/paste the whole URL.
+    /// </summary>
+    public class LinkModule : ModuleBase
+    {
+        [Command("twitch")]
+        public async Task PostTwitchLink(string channel)
+        {
+            await ReplyAsync($"https://www.twitch.tv/{channel}");
+        }
+    }
+}
