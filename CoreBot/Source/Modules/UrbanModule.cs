2321fcf762e770161ba85745c286a67dce2c77bf
eklypss <eklypssdev@gmail.com>
2017-08-16 18:09:46 +0300

    Change log message

--- CoreBot/Source/Modules/UrbanModule.cs
+++ CoreBot/Source/Modules/UrbanModule.cs
@@ -24,12 +24,12 @@ namespace CoreBot.Modules
             if (definitions.Count > 0)
             {
                 string response = string.Join(Environment.NewLine, definitions);
-                Log.Information($"Lenght: {response.Length}");
                 if (response.Length > 2000)
                 {
                     // If the message length for all definitions is too long, just display the first definition.
                     // TODO: Change it shows as many definitions as possible (max length is 2000 characters).
                     await ReplyAsync(definitions.FirstOrDefault());
+                    Log.Warning($"Response lenght for all definitions is too long, responding with the first definition only.");
                 }
                 else await ReplyAsync(response);
             }
