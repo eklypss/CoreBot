189cc4d1954bfdc367c1df2e01d1ca0ba0bdf1ed
mmorz <mmorz@users.noreply.github.com>
2017-08-30 00:19:24 +0300

    Use systemd for persistence

--- CoreBot.Scripts/deploy.sh
+++ CoreBot.Scripts/deploy.sh
@@ -14,12 +14,12 @@ then
     exit 1
 fi
 
-echo "Waiting for previous process"
-ssh -p $CORE_PORT $CORE_SERVER "fuser -s -k -SIGTERM publish/CoreBot || true"
-echo "Killed previous bot process"
+echo "waiting for previous process"
+ssh -p $CORE_PORT $CORE_SERVER "systemctl --user stop corebot"
+echo "stopped previous bot process"
 rsync -chrv -e 'ssh -p '$CORE_PORT $BUILD_DIR $CORE_SERVER:
-echo "New files copied"
+echo "new files copied"
 rm -rf $BUILD_DIR
 
-ssh -t -p $CORE_PORT $CORE_SERVER "tmux new-window 'publish/CoreBot' &&
-    tmux attach || tmux new-session 'publish/CoreBot'"
+ssh -t -p $CORE_PORT $CORE_SERVER "systemctl --user start corebot \
+    && journalctl -f -o cat --user-unit corebot"
