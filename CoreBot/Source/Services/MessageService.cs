a68f7b0273567f40cf9f308e563a593f675978c7
eklypss <eklypssdev@gmail.com>
2017-09-01 14:57:32 +0300

    Add some error checking

--- CoreBot/Source/Services/MessageService.cs
+++ CoreBot/Source/Services/MessageService.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CoreBot.Interfaces;
 using CoreBot.Settings;
 using Discord;
+using Serilog;
 
 namespace CoreBot.Services
 {
@@ -17,10 +19,21 @@ namespace CoreBot.Services
 
         public async Task SendMessageToDefaultChannelAsync(string message)
         {
-            var guilds = await _client.GetGuildsAsync();
-            var chans = await guilds.FirstOrDefault(x => x.Name == BotSettings.Instance.DefaultGuild).GetTextChannelsAsync();
-            var channel = chans.FirstOrDefault(x => x.Name == BotSettings.Instance.DefaultChannel);
-            await channel.SendMessageAsync(message);
+            try
+            {
+                var guilds = await _client.GetGuildsAsync();
+                var chans = await guilds.FirstOrDefault(x => x.Name == BotSettings.Instance.DefaultGuild).GetTextChannelsAsync();
+                var channel = chans.FirstOrDefault(x => x.Name == BotSettings.Instance.DefaultChannel);
+                await channel.SendMessageAsync(message);
+            }
+            catch (NullReferenceException)
+            {
+                Log.Error("Can't send message to default channel since it has not been set.");
+            }
+            catch (Exception)
+            {
+                Log.Error($"Couldn't send message to {BotSettings.Instance.DefaultChannel} on {BotSettings.Instance.DefaultGuild}.");
+            }
         }
     }
 }
