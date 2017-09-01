5551b9e3a88cfd91660779b77f12ca28dd149431
mmorz <mmorz@users.noreply.github.com>
2017-09-01 13:49:39 +0300

    Catch missing channel exception

--- CoreBot/Source/Services/MessageService.cs
+++ CoreBot/Source/Services/MessageService.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CoreBot.Interfaces;
 using CoreBot.Settings;
 using Discord;
+using Serilog;
 
 namespace CoreBot.Services
 {
@@ -18,9 +20,20 @@ namespace CoreBot.Services
         public async Task SendMessageToDefaultChannelAsync(string message)
         {
             var guilds = await _client.GetGuildsAsync();
-            var chans = await guilds.FirstOrDefault(x => x.Name == BotSettings.Instance.DefaultGuild).GetTextChannelsAsync();
-            var channel = chans.FirstOrDefault(x => x.Name == BotSettings.Instance.DefaultChannel);
-            await channel.SendMessageAsync(message);
+            try
+            {
+                var chans = await guilds
+                    .First(x => x.Name == BotSettings.Instance.DefaultGuild)
+                    .GetTextChannelsAsync();
+
+                var channel = chans.First(x => x.Name == BotSettings.Instance.DefaultChannel);
+                await channel.SendMessageAsync(message);
+            }
+            catch (Exception)
+            {
+                Log.Error($"couldn't send message because channel '{BotSettings.Instance.DefaultChannel}'" +
+                    $" at '{BotSettings.Instance.DefaultGuild}' not found");
+            }
         }
     }
 }
