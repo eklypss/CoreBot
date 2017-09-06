2c28bf830253700df10c258fa194256970351f54
mmorz <mmorz@users.noreply.github.com>
2017-09-06 18:59:26 +0300

    Revert "Two-part name fetching for last seen command"
    This reverts commit 6747ce684e6c0c896a43be7f3cf2aee2bb148c9f.

--- CoreBot/Source/Helpers/HelperMethods.cs
+++ CoreBot/Source/Helpers/HelperMethods.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Discord;
-using Discord.WebSocket;
 
 namespace CoreBot.Helpers
 {
@@ -16,31 +12,6 @@ namespace CoreBot.Helpers
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return epoch.AddSeconds(unixTime);
-        }
-
-        /// <summary>
-        /// Find user by nickname with username fallback. Case-insensitive.
-        /// </summary>
-        /// <param name="channel"></param>
-        /// <param name="nick"></param>
-        /// <exception cref="InvalidOperationException">No user found</exception>
-        public async static Task<IUser> FindUserByNameAsync(this IMessageChannel channel,
-            string nick)
-        {
-            var u = await channel.GetUsersAsync().Flatten();
-            var users = u.Cast<SocketGuildUser>();
-
-            var lowerNick = nick.ToLower();
-
-            try
-            {
-                return users.First(sender => sender.Nickname.ToLower() == lowerNick);
-            }
-            catch (InvalidOperationException)
-            {
-                // Throw if not found even with username
-                return users.First(sender => sender.Username.ToLower() == lowerNick);
-            }
         }
     }
 }
--- CoreBot/Source/Modules/SeenModule.cs
+++ CoreBot/Source/Modules/SeenModule.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
-using CoreBot.Helpers;
 using CoreBot.Settings;
-using Discord;
 using Discord.Commands;
 using Humanizer;
 
@@ -12,55 +10,20 @@ namespace CoreBot.Modules
     public class SeenModule : ModuleBase
     {
         [Command("seen"), Summary("Shows latest activity of the specified user.")]
-        public async Task GetUserLastSeenInfoAsync(string name)
+        public async Task GetUserLastSeenInfoAsync(string userName)
         {
-            IUser user;
-
-            try
+            var found = await Context.Channel.GetMessagesAsync(2000)
+                .FirstOrDefault(batch => batch
+                .Any(message => message.Author.Username.Equals(userName, StringComparison.InvariantCultureIgnoreCase)));
+            if (found != null)
             {
-                user = await Context.Channel.FindUserByNameAsync(name);
+                var msg = found.First(m => m.Author.Username.Equals(userName, StringComparison.InvariantCultureIgnoreCase));
+                await ReplyAsync($"{msg.Author.Username} was last seen {DateTime.Now.Subtract(msg.Timestamp.DateTime).Humanize(BotSettings.Instance.HumanizerPrecision)} ago saying: `{msg.Content}`");
             }
-            catch (InvalidOperationException)
+            else
             {
-                await ReplyAsync($"No user with nick or username '{name}'");
-                return;
+                await ReplyAsync($"No messages from \"{userName}\"");
             }
-
-            IMessage msg;
-
-            try
-            {
-                msg = await LatestMessageAsync(user);
-            }
-            catch (InvalidOperationException)
-            {
-                await ReplyAsync($"No message from '{name}' in 2000 messages");
-                return;
-            }
-
-            var timeDiff = DateTime.UtcNow - msg.Timestamp;
-
-            await ReplyAsync($"{name} was last seen " +
-                $"{timeDiff.Humanize(BotSettings.Instance.HumanizerPrecision)} " +
-                $"ago saying: `{msg.Content}`");
-        }
-
-        /// <summary>
-        /// Find last message for user
-        /// </summary>
-        /// <param name="user">user to search for</param>
-        /// <exception cref="InvalidOperationException">
-        /// User hasn't sent message in last 2000 messages
-        /// </exception>
-        private async Task<IMessage> LatestMessageAsync(IUser user)
-        {
-            var foundBatch = await Context.Channel.GetMessagesAsync(2000)
-                .First(batch => batch.Any(message =>
-                (
-                    message.Author.Id == user.Id
-                )));
-
-            return foundBatch.First(message => message.Author.Id == user.Id);
         }
     }
 }
