167ea1b0e017ba48d78095354be6f0b90b739284
Alex <dwt416@gmail.com>
2018-01-10 00:23:34 +0200

    Improve naming

--- CoreBot/Source/Modules/NHLModule.cs
+++ CoreBot/Source/Modules/NHLModule.cs
@@ -19,18 +19,18 @@ namespace CoreBot.Modules
         public async Task GetStandingsAsync()
         {
             var standings = await _nhlService.GetStandingsAsync();
-            var standing = new List<string>();
+            var standingStringList = new List<string>();
 
             foreach (var conference in standings.Records)
             {
                 conference.TeamRecords.Sort((a, b) => b.Points.CompareTo(a.Points));
-                standing.Add($"**{conference.Conference.Name} {conference.Division.Name}**");
+                standingStringList.Add($"**{conference.Conference.Name} {conference.Division.Name}**");
 
                 foreach (var team in conference.TeamRecords)
                 {
-                    standing.Add($"{team.Team.Name} **GP**: {team.GamesPlayed} **W:** {team.LeagueRecord.Wins} **L:** {team.LeagueRecord.Losses} **OT:** {team.LeagueRecord.Ot} **GF:** {team.GoalsScored} **GA:** {team.GoalsAgainst} **DIFF:** {team.GoalsScored - team.GoalsAgainst} **TP:** {team.Points}");
+                    standingStringList.Add($"{team.Team.Name} **GP**: {team.GamesPlayed} **W:** {team.LeagueRecord.Wins} **L:** {team.LeagueRecord.Losses} **OT:** {team.LeagueRecord.Ot} **GF:** {team.GoalsScored} **GA:** {team.GoalsAgainst} **DIFF:** {team.GoalsScored - team.GoalsAgainst} **TP:** {team.Points}");
                 }
-                await ReplyAsync(string.Join(Environment.NewLine, standing));
+                await ReplyAsync(string.Join(Environment.NewLine, standingStringList));
             }
         }
     }
