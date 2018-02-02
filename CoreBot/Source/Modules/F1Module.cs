e08c7b9b1cdcd5062106933c030cfdba243681a3
Alex <dwt416@gmail.com>
2018-02-02 07:31:26 +0200

    Remove unused using

--- CoreBot/Source/Modules/F1Module.cs
+++ CoreBot/Source/Modules/F1Module.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using CoreBot.Models.F1.Races;
 using CoreBot.Services;
 using CoreBot.Settings;
 using Discord.Commands;
