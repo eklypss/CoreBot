79fdc13f04ba2357228c0400be9508bad84fa266
Alex <dwt416@gmail.com>
2018-02-03 16:29:28 +0200

    Add another check for results

--- CoreBot/Source/Modules/WolframModule.cs
+++ CoreBot/Source/Modules/WolframModule.cs
@@ -24,7 +24,7 @@ namespace CoreBot.Modules
                 await ReplyAsync("No answer found.");
                 return;
             }
-            var resultPod = answer.Pods.FirstOrDefault(x => x.Title.ToLower().Contains("response") || x.Title.ToLower().Contains("result"));
+            var resultPod = answer.Pods.FirstOrDefault(x => x.Title.ToLower().Contains("response") || x.Title.ToLower().Contains("result") || x.Title.ToLower().Contains("summary"));
             var inputPod = answer.Pods.FirstOrDefault(x => x.Title.ToLower().Contains("input"));
             var embed = new EmbedBuilder()
                 .AddField(inputPod.Subpods.FirstOrDefault().PlainText, resultPod.Subpods.FirstOrDefault().PlainText);
