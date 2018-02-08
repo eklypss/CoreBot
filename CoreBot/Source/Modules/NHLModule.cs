52e24e037e8b91a8d19ac4c6276f8738d997a82c
Alex <dwt416@gmail.com>
2018-02-08 13:18:32 +0200

    Use embedded messages for standings

--- CoreBot/Source/Modules/NHLModule.cs
+++ CoreBot/Source/Modules/NHLModule.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using CoreBot.Services;
+using CoreBot.Settings;
+using Discord;
 using Discord.Commands;
 
 namespace CoreBot.Modules
@@ -21,15 +23,15 @@ namespace CoreBot.Modules
             var standings = await _nhlService.GetStandingsAsync();
             foreach (var conference in standings.Records)
             {
+                var embed = new EmbedBuilder().WithColor(BotSettings.Instance.EmbeddedColor);
                 var standingStringList = new List<string>();
                 conference.TeamRecords.Sort((a, b) => b.Points.CompareTo(a.Points));
-                standingStringList.Add($"**{conference.Conference.Name} {conference.Division.Name}**");
-
                 foreach (var team in conference.TeamRecords)
                 {
                     standingStringList.Add($"{team.Team.Name} **GP**: {team.GamesPlayed} **W:** {team.LeagueRecord.Wins} **L:** {team.LeagueRecord.Losses} **OT:** {team.LeagueRecord.Ot} **GF:** {team.GoalsScored} **GA:** {team.GoalsAgainst} **DIFF:** {team.GoalsScored - team.GoalsAgainst} **TP:** {team.Points}");
                 }
-                await ReplyAsync(string.Join(Environment.NewLine, standingStringList));
+                embed.AddField($"**{conference.Conference.Name} {conference.Division.Name}**", string.Join(Environment.NewLine, standingStringList));
+                await ReplyAsync(string.Empty, embed: embed);
             }
         }
     }
